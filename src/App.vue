72d86a33def0fac6145495085877e29c72ed452d
LinKhantNyiNyi <linnk5860@gmail.com>
2023-08-10 00:48:04 +0630

    Check win lose and display result

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1>Let's guess Head or Tail</h1>
+  <h2>Let's guess Head or Tail</h2>
   <Coin :isPlaying="isPlaying" :result="result" />
   <button class="play-btn" @click="startGame" v-if="isPlaying === false">
     Play
   </button>
+  <p>{{ message }}</p>
+  <p v-if="Win">
+    You win. It's <span v-if="result == 'H'">Head</span
+    ><span v-if="result == 'T'">Tail</span>
+  </p>
+  <p v-if="Win == false">
+    You lose It's <span v-if="result == 'H'">Head</span
+    ><span v-if="result == 'T'">Tail</span>
+  </p>
   <Guess v-if="isPlaying" @guess="guess" />
 </template>
 
@@ -22,22 +31,40 @@ export default {
     return {
       isPlaying: false,
       result: "",
+      Win: null,
+      message: "",
     };
   },
 
   methods: {
     startGame() {
       this.isPlaying = true;
+      this.result = "";
+      this.Win = null;
     },
 
     guess(guessedSide) {
+      this.message = "Wait for it..";
       setTimeout(() => {
         this.CheckWinLose(guessedSide);
       }, 6000);
     },
 
     CheckWinLose(guessedSide) {
-      console.log(guessedSide);
+      let x = Math.random() * 2 + 1;
+      let y = Math.floor(x);
+      if (y === guessedSide) {
+        this.Win = true;
+      } else {
+        this.Win = false;
+      }
+      if (y === 1) {
+        this.result = "H";
+      } else {
+        this.result = "T";
+      }
+      this.isPlaying = false;
+      this.message = "";
     },
   },
 };
